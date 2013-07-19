136cbc516db18f42d60f9b61566ee1e99a81fa17
Kirit Sælensminde <k@kirit.com>
2013-07-19 09:27:58 +0700

    Initial stab at question 2

---
+++ q2/solution.cpp
@@ -0,0 +1,27 @@
+/* Compile with:
+ *      clang++ -std=c++11 -O3 solution.cpp
+ * Execute with:
+ *      ./a.out
+ */
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+
+int main() {
+    std::ifstream data("numbers.txt");
+    std::vector<int> numbers(1000000, 0);
+    while ( not data.eof() ) {
+        int number;
+        data >> number;
+        ++numbers[number];
+    }
+    for ( int index = 0; index < numbers.size(); ++index ) {
+        if ( numbers[index] > 1 ) {
+            std::cout << std::setfill('0') << std::setw(6);
+            std::cout << index << std::endl;
+        }
+    }
+}
+
